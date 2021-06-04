9fb024fe3563b0b6ce0f2c21d5d64977f8c063c0
Jeremy Mendez <iamjeremymendez234@gmail.com>
2021-06-04 13:46:07 +0800

    added Form1_Load and System.IO to the code

--- Mendez_ContactTracing/Form1.cs
+++ Mendez_ContactTracing/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -47,6 +48,11 @@ namespace Mendez_ContactTracing
 
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
         private void btnExit_Click(object sender, EventArgs e)
         {
             this.Close();
@@ -75,5 +81,7 @@ namespace Mendez_ContactTracing
         {
 
         }
+
+      
     }
 }
